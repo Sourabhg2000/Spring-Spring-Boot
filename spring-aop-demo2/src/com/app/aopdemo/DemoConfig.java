c46da1791b4591b30c6e867a8fb249ca64d1ebec
Sourabh Gaikwad <sourabhg9009@gmail.com>
2023-12-23 20:25:43 +0530

    AOP Demo3

---
+++ spring-aop-demo2/src/com/app/aopdemo/DemoConfig.java
@@ -0,0 +1,28 @@
+package com.app.aopdemo;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.app.aopdemo.dao.AccountDAO;
+import com.app.aopdemo.dao.MembershipDAO;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.app.aopdemo")
+public class DemoConfig {
+	
+	@Bean(name="bean1")
+	public AccountDAO accDao()
+	{
+		return new AccountDAO();
+	}
+	
+	@Bean(name="bean2")
+	public MembershipDAO memDao()
+	{
+		return new MembershipDAO();
+	}
+}
